40340b676df360f0c8f6788ea7960789b5e3bd52
HeaganLee <ekzmfkdl12@gmail.com>
2023-04-18 18:23:06 +0900

    loginForm

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import { Reset } from "./styles/Global/reset";
+import { Global } from "@emotion/react";
+import Login from "./Pages/Login/Login";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Global styles={ Reset }></Global>
+      <Routes>
+        <Route exact path="/login" Component={Login} />
+        <Route path="/register" Component={Register} />
+      </Routes>
+    </>
   );
 }
 
