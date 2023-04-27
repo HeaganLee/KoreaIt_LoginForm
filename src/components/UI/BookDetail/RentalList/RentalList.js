98e3ecbac2000a492aa9bd547cb9ffe714020134
HeaganLee <ekzmfkdl12@gmail.com>
2023-04-27 17:39:15 +0900

    대여구현

--- src/components/UI/BookDetail/RentalList/RentalList.js
+++ src/components/UI/BookDetail/RentalList/RentalList.js
@@ -25,26 +25,49 @@ const RentalList = ({ bookId }) => {
         }
         return await axios.get(`http://localhost:8080/book/${bookId}/rental/list`, option);
     });
+
     
-    const rentalBook = useMutation(["rentalBook"], async (bookListId) => {
+    const rentalBook = useMutation( async (bookListId) => {
         const option = {
             headers: {
                 "Content-type": "application/json",
                 Authorization: localStorage.getItem("accessToken")
             }
         }
-        return await axios.post(`http://localhost:8080/book/${bookListId}/rental`, JSON.stringify({
+        return await axios.post(`http://localhost:8080/book/rental/${bookListId}`, JSON.stringify({
             userId: queryClient.getQueryData("principal").data.userId
-        }) ,option);
+        }) ,option)
         
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries("getRentalList");
+        }
+    });
+
+    const returnBook = useMutation( async (bookListId) => {
+        const option = {
+            params: {
+                userId: queryClient.getQueryData("principal").data.userId 
+            },
+            headers: {
+                Authorization: localStorage.getItem("accessToken")
+            }
+        }
+        return await axios.delete(`http://localhost:8080/book/return/${bookListId}`, option)
+    }, {
+        onSuccess: () => {
+            queryClient.invalidateQueries("getRentalList");
+        }
     })
+
+
     
     if(getRentalList.isLoading) {
         <div>...로딩중</div>
     }
 
 
-    // const returnBook = 
+    
 
     if(!getRentalList.isLoading)
     return (
@@ -63,9 +86,9 @@ const RentalList = ({ bookId }) => {
                             <td css={thAndTd}>{rentalData.bookListId}</td>
                             <td css={thAndTd}>{rentalData.bookName}</td>
                             {rentalData.rentalStatus 
-                                ? (<td css={thAndTd}>대여가능 <button onClick={rentalBook.mutate(rentalData.bookListId)}>대여</button></td>) 
+                                ? (<td css={thAndTd}>대여가능 <button onClick={() => {rentalBook.mutate(rentalData.bookListId)}}>대여</button></td>) 
                                 : (<td css={thAndTd}>대여중 {rentalData.userId === queryClient.getQueryData("principal").data.userId 
-                                    ? <button>반납</button> : ""} </td>)}
+                                    ? <button onClick={() => {returnBook.mutate(rentalData.bookListId)}}>반납</button> : ""} </td>)}
                         </tr>)
                     })}
                 </tbody>
