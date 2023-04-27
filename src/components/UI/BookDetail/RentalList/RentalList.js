45d466f20c46595d3fe45aa3fa52ff0e2564302a
HeaganLee <ekzmfkdl12@gmail.com>
2023-04-27 16:00:18 +0900

    no message

--- src/components/UI/BookDetail/RentalList/RentalList.js
+++ src/components/UI/BookDetail/RentalList/RentalList.js
@@ -25,14 +25,24 @@ const RentalList = ({ bookId }) => {
         }
         return await axios.get(`http://localhost:8080/book/${bookId}/rental/list`, option);
     });
-
+    
+    const rentalBook = useMutation(["rentalBook"], async (bookListId) => {
+        const option = {
+            headers: {
+                "Content-type": "application/json",
+                Authorization: localStorage.getItem("accessToken")
+            }
+        }
+        return await axios.post(`http://localhost:8080/book/${bookListId}/rental`, JSON.stringify({
+            userId: queryClient.getQueryData("principal").data.userId
+        }) ,option);
+        
+    })
+    
     if(getRentalList.isLoading) {
         <div>...로딩중</div>
     }
 
-    const retalBook = useMutation(async () => {
-        
-    })
 
     // const returnBook = 
 
@@ -53,7 +63,7 @@ const RentalList = ({ bookId }) => {
                             <td css={thAndTd}>{rentalData.bookListId}</td>
                             <td css={thAndTd}>{rentalData.bookName}</td>
                             {rentalData.rentalStatus 
-                                ? (<td css={thAndTd}>대여가능 <button>대여</button></td>) 
+                                ? (<td css={thAndTd}>대여가능 <button onClick={rentalBook.mutate(rentalData.bookListId)}>대여</button></td>) 
                                 : (<td css={thAndTd}>대여중 {rentalData.userId === queryClient.getQueryData("principal").data.userId 
                                     ? <button>반납</button> : ""} </td>)}
                         </tr>)
